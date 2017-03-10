3d0bf8fc5cc96b2cfeed9fb241a05ef4a1807981
Brennan McFarland <bfm21@case.edu>
2017-03-09 19:25:34 -0500

    added update class stub

---
+++ Update.java
@@ -0,0 +1,76 @@
+/**
+ * @author Brennan McFarland
+ * Update handles all updates to the documentation database, both automatic
+ * and manual, including error checking and handling, logging, and formatting
+ * downloaded documentation
+ * */
+public class Update
+{
+  boolean autoUpdate;
+  ManPage[] docs;
+
+  public void checkAutoUpdateCondition()
+  {
+
+  }
+
+  public void update(boolean isManual)
+  {
+
+  }
+
+  private void checkInternetConnection()
+  {
+
+  }
+
+  private void downloadDocs()
+  {
+
+  }
+
+  private void downloadDoc(String url)
+  {
+
+  }
+
+  private void formatDocs()
+  {
+
+  }
+
+  private void formatDoc()
+  {
+
+  }
+
+  private void updateLocalDocs()
+  {
+
+  }
+
+  private void updateLocalDoc()
+  {
+
+  }
+
+  private void storeIncompleteUpdatedDocs()
+  {
+
+  }
+
+  private void storeIncompleteUpdatedDoc()
+  {
+
+  }
+
+  private void logUpdate()
+  {
+
+  }
+
+  private void displayErrorNoInternet()
+  {
+
+  }
+}
