a3317316450c90fad10a3f0b0eb6163ec13b1ff1
Jonathan Duffy <jcduffy5@gmail.com>
2017-03-09 22:19:38 -0500

    output .json file (500 classes)

--- docScraper.js
+++ docScraper.js
@@ -1,5 +1,6 @@
 var casper = require('casper').create();
 var x = require('casper').selectXPath;
+var fs = require('fs');
 
 function Method() {
 	this.name = "";
@@ -54,45 +55,37 @@ casper.waitForText('All Classes', function(){
 }, 10000);
 
 casper.then(function(){
-	casper.repeat(classes.length, function(){
+	casper.repeat(/*classes.length*/ 500, function(){
 		this.thenOpen(rootUrl + classes[i].href, function(){
 			var constIndex = 2;
-			var constructor = this.fetchText(x('/html/body/div[4]/div[2]/ul/li/ul[2]/li/table/tbody/tr[' + constIndex + ']/td/code')).replace(/\u00a0/g, " ");
-			var description = this.fetchText(x('/html/body/div[4]/div[2]/ul/li/ul[2]/li/table/tbody/tr[' + constIndex + ']/td/div')).replace(/\u00a0/g, " ");
-			//console.log(classes[i].name + ": " + constructor + ": " + description);
-			while(constructor != null){
+			while(this.exists(x('/html/body/div[4]/div[2]//a[@name="constructor.summary"]/../table/tbody/tr[' + constIndex + ']/td/code'))){
+				constructor = this.fetchText(x('/html/body/div[4]/div[2]//a[@name="constructor.summary"]/../table/tbody/tr[' + constIndex + ']/td/code')).replace(/\u00a0/g, " ");
+				description = this.fetchText(x('/html/body/div[4]/div[2]//a[@name="constructor.summary"]/../table/tbody/tr[' + constIndex + ']/td/div')).replace(/\u00a0/g, " ");
 				constObj = new Constructor();
 				constObj.name = constructor;
 				constObj.description = description;
 				classes[i].constructors.push(constObj);
 				constIndex++;
-				if(this.exists(x('/html/body/div[4]/div[2]/ul/li/ul[2]/li/table/tbody/tr[' + constIndex + ']/td/code'))){
-					constructor = this.fetchText(x('/html/body/div[4]/div[2]/ul/li/ul[2]/li/table/tbody/tr[' + constIndex + ']/td/code')).replace(/\u00a0/g, " ");
-					description = this.fetchText(x('/html/body/div[4]/div[2]/ul/li/ul[2]/li/table/tbody/tr[' + constIndex + ']/td/div')).replace(/\u00a0/g, " ");
-					//console.log(constructor + ": " + description);
-				}
-				else{
-					constructor = null;
-				}
-
 			}
+			
 			var methIndex = 0;
 			var method = this.fetchText(x('//*[@id="i'+ methIndex +'"]/td[2]/code')).replace(/\u00a0/g, " ");
 			var mdescription = this.fetchText(x('//*[@id="i'+ methIndex +'"]/td[2]/div')).replace(/\u00a0/g, " ");
 			var modAndType = this.fetchText(x('//*[@id="i'+ methIndex +'"]/td[1]')).replace(/\u00a0/g, " ");
 			//console.log(modAndType + ": "+ method + ": " + mdescription);
 			while(method != null){
-				methObj = new Method();
-				methObj.name = method;
-				methObj.description = mdescription;
-				methObj.modAndType = modAndType;
-				classes[i].methods.push(methObj);
-				methIndex++;
+				
 				if(this.exists(x('//*[@id="i'+ methIndex +'"]'))){
 					method = this.fetchText(x('//*[@id="i'+ methIndex +'"]/td[2]/code')).replace(/\u00a0/g, " ");
 					mdescription = this.fetchText(x('//*[@id="i'+ methIndex +'"]/td[2]/div')).replace(/\u00a0/g, " ");
 					modAndType = this.fetchText(x('//*[@id="i'+ methIndex +'"]/td[1]')).replace(/\u00a0/g, " ");
 					//console.log(modAndType +": "+ method + ": " + mdescription);
+					methObj = new Method();
+					methObj.name = method;
+					methObj.description = mdescription;
+					methObj.modAndType = modAndType;
+					classes[i].methods.push(methObj);
+					methIndex++;
 				}
 				else{
 					method = null;
@@ -101,10 +94,15 @@ casper.then(function(){
 			if(i%100 == 0){
 				console.log("Populated methods for " + i + " classes so far...");
 			}
-		});
-		i++;
+			i++;
+		});	
 	});
 	console.log("All methods populated.");
+});
+
+casper.then(function(){
+	classesString = JSON.stringify(classes, null, 4);
+	fs.write("Test.json", classesString, 'w');
 });
 
 casper.run(function(){
