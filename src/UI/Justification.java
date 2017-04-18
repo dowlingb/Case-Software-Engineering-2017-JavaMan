a12526b95fb7353f054a7cd73ed66350f1bfbc2d
easonback26 <easonback@gmail.com>
2017-04-18 12:01:07 -0400

    Add files via upload

---
+++ src/UI/Justification.java
@@ -0,0 +1,8 @@
+package UI;
+/**
+ * @author Brennan McFarland
+ * for formatting: left, center, or right justified
+ * */
+public enum Justification {
+	LEFT, CENTER, RIGHT, FIRSTINDENT, SECONDINDENT
+}
