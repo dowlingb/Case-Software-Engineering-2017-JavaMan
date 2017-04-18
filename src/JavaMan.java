7467ee2f0f3928b615c4bc174bca45c99429c59b
easonback26 <easonback@gmail.com>
2017-04-18 19:42:31 -0400

    Add files via upload

---
+++ src/JavaMan.java
@@ -0,0 +1,190 @@
+/*
+ * @author Brendan Dowling
+ * This is the main class for accessing and running the javaman program
+ * */
+  import java.io.*;
+  import java.io.FileNotFoundException;
+  import java.io.FileReader;
+  import java.io.IOException;
+  import org.json.simple.JSONArray;
+  import org.json.simple.JSONObject;
+  import org.json.simple.parser.JSONParser;
+  import org.json.simple.parser.ParseException;
+  
+public class JavaMan
+{
+  public static String home = File.listRoots()[0].toString();//hypothetically should work indepentant of operating system as long as the files are in the home folder
+  //"c:" + File.separatorChar; <for windows
+  public static JSONArray classArray;
+  public static String correctOutputFormatMessage = "The correct format for printing "
+		  + "commands is blah blah blah";
+  public static String noInternetConnectionMessage = "There is no network connection.  "
+		  + "Please connect to the internet so updates can complete.";
+  
+  //added a new boolean isUI to determine if it's in UI.   @author Yiming Pan. 
+  private static boolean isUI = false;
+  private static userInterface ui = null;
+  
+  public static void main(String[] args) {
+   init();
+   if(args==null||args.length == 0 || args[0] == null)
+   {
+     notRecognized();
+     return;
+   }
+   else
+   {
+    if(args[0].equals("update"))
+    {
+     update();
+     return;
+    }
+    else if(args[0].equals("help"))
+    {
+      help();
+      return;
+    }
+    else if(args[0].equals("access")&&args.length>=2)
+    {
+      access(args[1]);
+      return;
+    }
+    else if(args[0].equals("accessclass") && args.length>=2)
+    {
+    	accessClass(args[1]);
+    	return;
+    }
+    else
+    {
+      notRecognized();
+    }
+   }
+  }
+
+  public static String help()
+  {
+   String message = "Enter command to update using \"update\" or access a class using \"access <class path>\" where class path is formatted as java.lang.String";
+   System.out.println(message);
+   return message;
+	  
+  }
+
+  public static void notRecognized()
+  {
+   String message = "Command not recognized.";
+   System.out.println(message);
+   //return message;
+  }
+  public static void access(String path)
+  {
+    String filePath = path.replaceAll("\\.","/");
+    filePath=filePath+".html";
+	 for(Object o : classArray)
+	 {
+	  JSONObject jClass = (JSONObject) o;
+	  //System.out.println(jClass.get("name"));
+	  if(jClass.get("href").toString().equals(filePath)){
+		  print("Class: ");
+		  print("\t" + jClass.get("name"));
+  		  JSONArray jConstructors = (JSONArray) jClass.get("constructors");
+  		  print("Constructors: ");
+  		  for(Object b : jConstructors)
+  		  {
+  			  JSONObject jConstObj = (JSONObject) b;
+  			  String constructorName = (String)jConstObj.get("name");
+  			  String constructorDesc = (String)jConstObj.get("description");
+  			  print("\t" + (constructorName + " : " + constructorDesc).replaceAll("\n|\t| (?= )", ""));
+  		  }
+  		  
+  		  JSONArray jMethods = (JSONArray) jClass.get("methods");
+  		  System.out.println("Methods: ");
+  		  for(Object b : jMethods)
+  		  {
+  			  JSONObject jMethodObj = (JSONObject) b;
+  			  String methodName = (String) jMethodObj.get("name");
+  			  String methodDesc = (String) jMethodObj.get("description");
+  			  String modAndType = (String) jMethodObj.get("modAndType");
+  			  print("\t" + (methodName + " : " + methodDesc).replaceAll("\t|\n| (?= )", ""));
+  			  print("\t\t Returns: " + (modAndType).replaceAll("\n|\t| (?= )", ""));
+  		  }
+		 }
+	  }
+	  
+  }
+  
+  public static void accessClass(String classStr){
+	  for(Object o : classArray)
+	  {
+		  JSONObject jClass = (JSONObject) o;
+		  //System.out.println(jClass.get("name"));
+		  if(jClass.get("name").toString().equals(classStr)){
+			  print("Class: ");
+			  print("\t" + jClass.get("name"));
+
+    		  JSONArray jConstructors = (JSONArray) jClass.get("constructors");
+    		  print("Constructors: ");
+    		  for(Object b : jConstructors)
+    		  {
+    			  JSONObject jConstObj = (JSONObject) b;
+    			  String constructorName = (String)jConstObj.get("name");
+    			  String constructorDesc = (String)jConstObj.get("description");
+    			  print("\t" + (constructorName + " : " + constructorDesc).replaceAll("\n|\t| (?= )", ""));
+    		  }
+    		  
+    		  JSONArray jMethods = (JSONArray) jClass.get("methods");
+    		  System.out.println("Methods: ");
+    		  for(Object b : jMethods)
+    		  {
+    			  JSONObject jMethodObj = (JSONObject) b;
+    			  String methodName = (String) jMethodObj.get("name");
+    			  String methodDesc = (String) jMethodObj.get("description");
+    			  String modAndType = (String) jMethodObj.get("modAndType");
+    			  print("\t" + (methodName + " : " + methodDesc).replaceAll("\t|\n| (?= )", ""));
+    			  print("\t\t Returns: " + (modAndType).replaceAll("\n|\t| (?= )", ""));
+    		  }
+		  }
+	  }
+  }
+
+  public static void update()
+  {
+    Update.update(true);
+  }
+  
+  private static void print(String msg){
+	  if(isUI==false){
+		  System.out.println(msg);
+	  }
+	  else{
+		  ui.print(msg);
+	  }
+	}
+  
+  public void setUI(userInterface importedUI){
+	  isUI = true;
+	  ui = importedUI;
+	  Update.setUI(importedUI);
+  }
+  
+  public static void init()
+  {
+	  try
+      {
+    	  JSONParser parser = new JSONParser();
+    	  classArray = (JSONArray) parser.parse(new FileReader("Test.json"));
+      } catch (FileNotFoundException e)
+      {
+          e.printStackTrace();
+      } catch (IOException e)
+      {
+          e.printStackTrace();
+      } catch (ParseException e)
+      {
+          e.printStackTrace();
+      }
+    }
+  
+  	public JavaMan(){
+  		init();
+  	}
+  }
---
+++ src/userInterface.java
@@ -0,0 +1,182 @@
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import static javax.swing.GroupLayout.Alignment.*;
+
+public class userInterface extends JFrame {
+	
+	private final static String newline = "\n";
+	
+	private JavaMan jm;
+    private JMenuBar menuBar;
+    private JMenu optionMenu;
+    private JMenuItem help;
+    private JMenuItem update;
+    
+    private JLabel accLabel;
+    private JTextField accText;
+    private JButton goButton1;
+    
+    private JLabel acLabel;
+    private JTextField acText;
+    private JButton goButton2;
+    
+    
+    private JTextArea textArea;
+    private JScrollPane scroller;
+    
+
+    
+    public userInterface() {
+
+    	jm = new JavaMan();
+    	jm.setUI(this);
+    	//jm.init();
+    	
+    	buildMenuBar();
+    	
+    	accLabel = new JLabel("Access Class:");;
+        accText = new JTextField(20);
+        goButton1 = new JButton("Go");
+        goButton1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+          	  accessClass();
+            }
+         });
+        
+        acLabel = new JLabel("Access:");
+        acText = new JTextField(20);
+        goButton2 = new JButton("Go");
+        goButton2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+          	  access();
+            }
+         });
+        
+        textArea = new JTextArea(50, 150);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setEditable(false);
+        
+        scroller = new JScrollPane(textArea);
+
+        
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+        
+        layout.setHorizontalGroup(layout.createSequentialGroup()
+        		.addGroup(layout.createParallelGroup(LEADING)
+        				.addGroup(layout.createParallelGroup(LEADING)
+        						.addComponent(scroller))
+        		.addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(LEADING)
+                        		.addComponent(accLabel)
+                        		.addComponent(acLabel))
+                        .addGroup(layout.createParallelGroup(LEADING)
+                        		.addComponent(accText)
+                        		.addComponent(acText))
+                        .addGroup(layout.createParallelGroup(LEADING)
+                        		.addComponent(goButton1)
+        						.addComponent(goButton2))))
+           );
+        
+        layout.setVerticalGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(BASELINE)
+                	.addComponent(accLabel)
+                    .addComponent(accText)
+                    .addComponent(goButton1))
+                .addGroup(layout.createParallelGroup(LEADING)
+                    	.addComponent(acLabel)
+                        .addComponent(acText)
+                        .addComponent(goButton2))
+                .addGroup(layout.createParallelGroup(LEADING)
+                	.addComponent(scroller))
+            );
+        
+        setTitle("JAVAMAN USER INTERFACE");
+        pack();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+   
+    
+    private void accessClass(){
+    	jm.accessClass(accText.getText());
+    }
+    
+    private void access(){
+    	jm.access(acText.getText());
+    }
+    
+    public void print(String text){
+        textArea.append(text + newline);
+        accText.selectAll();
+
+        //Make sure the new text is visible, even if there
+        //was a selection in the text area.
+        textArea.setCaretPosition(textArea.getDocument().getLength());
+    }
+	
+    private void buildMenuBar()
+	{
+		// Build the file and font menus.
+		buildOptionMenu();
+
+		// Create the menu bar.
+		menuBar = new JMenuBar();
+
+		// Add the file and font menus to the menu bar.
+		menuBar.add(optionMenu);
+
+		// Set the menu bar for this frame.
+		setJMenuBar(menuBar);
+	}
+	
+	private void buildOptionMenu()
+	{
+	    // Create the Help menu item.
+		help = new JMenuItem("Help");
+		help.addActionListener(new helpListener());
+	    update = new JMenuItem("Update");
+	    update.addActionListener(new updateListener());
+	    
+	    optionMenu = new JMenu("Options");
+	    // Add the items and some separator bars to the menu.
+	    optionMenu.add(help);
+	    optionMenu.add(update);
+	
+	}
+	
+	private class helpListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			textArea.append("Enter command to update using \"update\" or access a class using \"access <class path>\" where class path is formatted as java.lang.String");
+		}
+	}
+	
+	private class updateListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			jm.update();
+		}
+	}
+	
+	
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel(
+                                  "javax.swing.plaf.metal.MetalLookAndFeel");
+                                //  "com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+                                //UIManager.getCrossPlatformLookAndFeelClassName());
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                new userInterface().setVisible(true);
+            }
+        });
+    }
+}
